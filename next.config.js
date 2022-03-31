22212f08d4ebdf079e75f7df77c0b1d98c1ec744
o.drapeza <o.drapeza@tinkoff.ru>
2022-04-01 00:10:07 +0300

    react 17

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // experimental: {
+  //   reactRoot: true,
+  //   runtime: 'nodejs',
+  // },
 }
 
 module.exports = nextConfig
